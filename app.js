99e03d6b06e76538add51f3ce46760ff53225de1
andreydnb <a.gritsay2@gmail.com>
2022-04-14 23:40:08 +0300

    update limiter

--- app.js
+++ app.js
@@ -2,23 +2,17 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit')
 const contactsRouter = require('./routes/api/contacts/contacts')
 const authRouter = require('./routes/api/auth/index')
+const limiter = require('./middlewares/rate-limiter')
+
 
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
-
 // Apply the rate limiting middleware to all requests
-app.use(limiter)
+app.use(limiter(15*60*1000,100))
 app.use(helmet());
 app.use(logger(formatsLogger))
 app.use(cors())
